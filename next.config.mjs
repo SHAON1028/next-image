e0dde173522a36418f479c7e052a8ab5dca5ddf3
khairul Islam <khairul.islam@adndiginet.com>
2024-05-28 11:18:02 +0600

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.adobe.com",
+        port: "",
+        pathname: "*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,14 @@ export default function Home() {
     <>
       <Image width={600} height={600} src="/right_arrow.svg" alt="tiger" />
       <div className="bg-image"></div>
-      <br />*
+      <br />
+
+      {/* <Image
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_pw1BpBfuZAevTRIg7RFQD1NZzdrEy016w&s"
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      /> */}
     </>
   );
 }
