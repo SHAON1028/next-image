101ec2eb3570a7c1a0d8c8bebaaeef09ff297b8b
khairul Islam <khairul.islam@adndiginet.com>
2024-05-28 11:22:11 +0600

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Image width={600} height={600} src="/right_arrow.svg" alt="tiger" />
+      <Image width={600} height={600} src="/tiger.jpg" alt="tiger" />
       <div className="bg-image"></div>
       <br />
 
