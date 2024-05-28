3bc06051bd3d15b068657343565f8fd794e145ec
khairul Islam <khairul.islam@adndiginet.com>
2024-05-28 10:50:56 +0600

    sds

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <>
       <Image width={600} height={600} src="/tiger.jpg" alt="tiger" />
+
+      <div className="bg-image">
+
+      </div>
     </>
   );
 }
